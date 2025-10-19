72996fe81057e1b5f060fa331056fadf4cbcaf25
root <root@Eduar.localdomain>
2025-10-18 19:40:02 -0500

    init project

---
+++ src/main/java/com/eduar_darin/stock_sys/StockSysApplication.java
@@ -0,0 +1,13 @@
+package com.eduar_darin.stock_sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockSysApplication.class, args);
+	}
+
+}
